b05ed9ca4b80637458c05aa2ffc3005df4b95e0e
Larissa <larissa@jm-contacts.net>
2025-10-23 09:25:44 +0300

    Premier commit lar

--- src/pages/paiement/PaiementManuel.jsx
+++ src/pages/paiement/PaiementManuel.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const PaiementManuel =()=> {
   return (
-    <div>
-       <strong>Paiement manuel</strong>
+    <div className="pManuel-page">
+      <div className="pManuel-header">
+        <h1 className="pManuel-title">DEMANDE DE PAIEMENT</h1>
+     <div>
+        <input
+            type="text"
+            placeholder="id client"
+            value=""
+            onChange={(e) => setName(e.target.value)}
+            required
+        />
+     </div>
+        
+      </div>
     </div>
   )
 }
